1f669fe7d6d18a9ad7731367ea838bfad2772ef4
o.dordiuk <o.dordiuk@viseven.com>
2022-03-31 17:29:26 +0300

    change github url

--- src/store/PersonalInfo.js
+++ src/store/PersonalInfo.js
@@ -3,7 +3,7 @@ export default {
     state: {
         myName: "Alex Dordiuk",
         mySocials: [
-            {socials: "Github", link: "https://github.com/AlexDordiuk/"},
+            {socials: "Github", link: "https://github.com/AlexDordiuk/vue-test-task"},
             {socials: "LinkedIn", link: "https://www.linkedin.com/in/aleksandrdordiuk/"},
             {socials: "Telegram", link: "https://telegram.me/Drawen77/"},
         ]
